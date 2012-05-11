233f27d794c1102f0085f1cdbb3e7c11309e8fab
Roberto Ostinelli <roberto@ostinelli.net>
2012-05-10 21:14:58 -0700

    seriously optimized parser

--- completion/sublimerl_libparser.py
+++ completion/sublimerl_libparser.py
@@ -26,7 +26,7 @@
 # POSSIBILITY OF SUCH DAMAGE.
 # ==========================================================================================================
 
-import sys, re, os, fnmatch, pickle
+import sys, re, os, fnmatch, pickle, string, unittest
 
 class SublimErlLibParser():
 
@@ -34,7 +34,10 @@ class SublimErlLibParser():
 		self.regex = {
 			'all': re.compile(r"(.*)", re.MULTILINE),
 			'export_section': re.compile(r"^\s*-\s*export\s*\(\s*\[\s*([^\]]*)\s*\]\s*\)\s*\.", re.DOTALL + re.MULTILINE),
-			'varname': re.compile(r"^[A-Z][a-zA-Z0-9_]*$")
+			'varname': re.compile(r"^[A-Z][a-zA-Z0-9_]*$"),
+			'{': re.compile(r"\{.*\}"),
+			'<<': re.compile(r"<<.*>>"),
+			'[': re.compile(r"\[.*\]")
 		}
 
 	def generate_completions(self, starting_dir, dest_file_base):
@@ -79,27 +82,10 @@ class SublimErlLibParser():
 		export_section = self.regex['export_section'].search(module)
 		if export_section == None: return []
 		# get list of exports
-		exports = self.get_list_of_exports(export_section)
+		exports = self.get_code_list_without_comments(export_section.group(1))
 		if len(exports) == 0: return []
 		# generate
 		return self.generate_module_completions(module, exports)
-
-	def get_list_of_exports(self, export_section):
-		# loop every line and add exports
-		all_exports = []
-		for m in self.regex['all'].finditer(export_section.group(1)):
-			groups = m.groups()
-			for i in range(0, len(groups)):
-				# strip away code comments
-				export = groups[i].strip().split('%')
-				# strip away empty lines
-				if len(export[0]) > 0:
-					exports = export[0].split(',')
-					for export in exports:
-						export = export.strip()
-						if len(export) > 0:
-							all_exports.append(export)
-		return all_exports
 
 	def generate_module_completions(self, module, exports):
 		completions = []
@@ -107,51 +93,176 @@ class SublimErlLibParser():
 			# split param count definition
 			fun = export.split('/')
 			if len(fun) == 2:
-				params = self.generate_params(module, fun)
+				params = self.generate_params(fun, module)
 				if params != None:
 					completions.append((export, '%s%s' % (fun[0].strip(), params)))
 		return completions
 
-	def generate_params(self, module, fun):
+	def generate_params(self, fun, module):
 		# get params count
-		try: count = int(fun[1])
-		except: return
-
-		# generate regex
-		params = []
-		for i in range(0, count): params.append(r"\s*([A-Z_][A-Za-z0-9_]*|.*)\s*")
-		regex = fun[0].strip() + r"\s*\(" + (",".join(params)) + r"\)\s*->"
-		regex = re.compile(regex, re.MULTILINE)
-
-		# loop matches
+		arity = int(fun[1])
+		# init
 		current_params = []
+		# get params
+		regex = re.compile(r"%s\((.*)\)" % fun[0], re.MULTILINE)
 		for m in regex.finditer(module):
-			if current_params != []:
-				groups = m.groups()
-				for i in range(0, len(groups)):
-					if not self.regex['varname'].search(current_params[i]):
-						# current param does not match a variable name
-						if self.regex['varname'].search(groups[i]):
-							# if current param is a variable name
-							current_params[i] = groups[i]
-			else:
-				current_params = list(m.groups())
-
+			params = m.groups()[0]
+			params = self.split_params(params)
+			if len(params) == arity:
+				# function definition has the correct arity
+				if current_params != []:
+					for i in range(0, len(params)):
+						if current_params[i] == '*' and self.regex['varname'].search(params[i]):
+							# found a valid variable name
+							current_params[i] = params[i]
+				else:
+					current_params = params
 		# ensure current params have variable names
 		for i in range(0, len(current_params)):
-			if not self.regex['varname'].search(current_params[i]):
+			if current_params[i] == '*':
 				current_params[i] = '${%d:Param%d}' % (i + 1, i + 1)
 			else:
 				current_params[i] = '${%d:%s}' % (i + 1, current_params[i])
-
+		# return
 		return '(' + ', '.join(current_params) + ') $%d' % (len(current_params) + 1)
+
+	def split_params(self, params):
+		# replace content of graffles with *
+		params = self.regex['{'].sub("*", params)
+		# replace content of <<>> with *
+		params = self.regex['<<'].sub("*", params)
+		# replace content of [] with *
+		params = self.regex['['].sub("*", params)
+		# take away comments and split per line
+		params = self.get_code_list_without_comments(params)
+		for p in range(0, len(params)):
+			# split on =
+			splitted_param = params[p].split('=')
+			if len(splitted_param) > 1:
+				params[p] = splitted_param[1].strip()
+			# convert to * where necessary
+			if not self.regex['varname'].search(params[p]):
+				params[p] = '*'
+		# return
+		return params
+
+	def get_code_list_without_comments(self, code):
+		# loop every line and add code lines
+		cleaned_code_list = []
+		for m in self.regex['all'].finditer(code):
+			groups = m.groups()
+			for i in range(0, len(groups)):
+				# strip away code comments
+				code_line = groups[i].strip().split('%')
+				if len(code_line[0]) > 0:
+					code_lines = code_line[0].split(',')
+					for code_line in code_lines:
+						code_line = code_line.strip()
+						if len(code_line) > 0:
+							cleaned_code_list.append(code_line)
+		return cleaned_code_list
+
+
+class TestSequenceFunctions(unittest.TestCase):
+
+	def setUp(self):
+		self.parser = SublimErlLibParser()
+
+	def test_split_params(self):
+		fixtures = [
+			("One, Two, Three", ["One", "Two", "Three"]),
+			("One", ["One"]),
+			("One, <<>>, Three", ["One", "*", "Three"]),
+			("One, [], Three", ["One", "*", "Three"]),
+			("One, {TwoA, TwoB}, Three", ["One", "*", "Three"]),
+			("One, {TwoA, TwoB, {TwoC, TwoD}}, Three", ["One", "*", "Three"]),
+			("One, {TwoA, TwoB, {TwoC, TwoD}} = Two, Three", ["One", "Two", "Three"]),
+			("One, {TwoA, TwoB, {TwoC, TwoD} = TwoE} = Two, Three", ["One", "Two", "Three"]),
+			("""% comment here
+				One,  % param one
+				Two,  % param two
+
+				Three % param three
+			 """, ["One", "Two", "Three"])
+		]
+		for f in range(0, len(fixtures)):
+			self.assertEqual(self.parser.split_params(fixtures[f][0]), fixtures[f][1])
+
+	def test_generate_params(self):
+		fixtures = [
+			(('start', '3'),"""
+							start(One, Two, Three) -> ok.
+
+							""", "(${1:One}, ${2:Two}, ${3:Three}) $4"),
+			(('start', '3'),"""
+							start(One, <<>>, Three) -> ok;
+							start(One, Two, Three) -> ok.
+
+							""", "(${1:One}, ${2:Two}, ${3:Three}) $4"),
+			(('start', '3'),"""
+							start(One, {Abc, Cde}, Three) -> ok;
+							start(One, Two, Three) -> ok.
+
+							""", "(${1:One}, ${2:Two}, ${3:Three}) $4"),
+			(('start', '3'),"""
+							start(One, <<Abc:16/binary, Cde/binary>>, Three) -> ok
+
+							""", "(${1:One}, ${2:Param2}, ${3:Three}) $4"),
+			(('start', '3'),"""
+							start(One, [Abc|R] = Two, Three) -> ok
+
+							""", "(${1:One}, ${2:Two}, ${3:Three}) $4"),
+			(('start', '3'),"""
+							start(One, [Abc|R], Three) -> ok
+
+							""", "(${1:One}, ${2:Param2}, ${3:Three}) $4"),
+			(('start', '3'),"""
+							start(One, [Abc, R], Three) -> ok
+
+							""", "(${1:One}, ${2:Param2}, ${3:Three}) $4"),
+			(('start', '3'),"""
+							start(One, Two, Three, Four) -> ok.
+							start(One, {Abc, Cde} = Two, Three) -> ok;
+							start(One, <<>>, Three) -> ok.
+
+							""", "(${1:One}, ${2:Two}, ${3:Three}) $4"),
+		]
+		for f in range(0, len(fixtures)):
+			self.assertEqual(self.parser.generate_params(fixtures[f][0], fixtures[f][1]), fixtures[f][2])
+
+	def test_get_completions(self):
+		fixtures = [
+			("""
+			-export([zero/0, one/1, two/2, three/3, four/4]).
+
+			zero() -> ok.
+			one(One) -> ok.
+			two(Two1, Two2) -> ok.
+			three(Three1, Three2, Three3) -> ok.
+			four(Four1, <<>>, Four3, Four4) -> ok;
+			four(Four1, {Four2A, Four2B, <<>>} = Four2, Four3, Four4) -> ok;
+			""",
+			[
+				('zero/0', 'zero() $1'),
+				('one/1', 'one(${1:One}) $2'),
+				('two/2', 'two(${1:Two1}, ${2:Two2}) $3'),
+				('three/3', 'three(${1:Three1}, ${2:Three2}, ${3:Three3}) $4'),
+				('four/4', 'four(${1:Four1}, ${2:Four2}, ${3:Four3}, ${4:Four4}) $5'),
+			])
+		]
+		for f in range(0, len(fixtures)):
+			self.assertEqual(self.parser.get_completions(fixtures[f][0]), fixtures[f][1])
 
 
 if __name__ == '__main__':
-	if (len(sys.argv) == 3):
+	if (len(sys.argv) == 2):
+		if sys.argv[1] == 'test':
+			sys.argv = [sys.argv[0]]
+			unittest.main()
+
+	elif (len(sys.argv) == 3):
 		starting_dir = sys.argv[1]
 		dest_file_base = sys.argv[2]
 		parser = SublimErlLibParser()
 		parser.generate_completions(starting_dir, dest_file_base)
-
 
